23a042e5dc1470a1a049a7ddb23de744422b3a0c
Andrew Alba <andrew.alba@gmail.com>
2024-11-11 21:41:30 -0600

    ♻️ (slug.vue): refactor layout styles for better responsiveness and readability ✨ (slug.vue): enhance ContentDoc component styling for improved text presentation

--- app/pages/legal/[slug].vue
+++ app/pages/legal/[slug].vue
@@ -14,8 +14,8 @@ const { data } = await useAsyncData("article", () =>
       <h1 class="text-6xl font-bold text-center text-gray-700 dark:text-white mb-4">{{ data.title }}</h1>
       <p class="text-center text-gray-500 dark:text-gray-300 italic">{{ data.last_updated_at }}</p>
     </div>
-    <div class="bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-100 max-w-7xl rounded-3xl flex flex-col items-center shadow-md -mt-20 mx-auto p-10 gap-10">
-      <ContentDoc :value="data" class="prose dark:prose-invert max-w-none" />
+    <div class="bg-white w-full sm:max-w-7xl dark:bg-gray-800 text-gray-700 dark:text-gray-100 rounded-3xl flex flex-col items-center shadow-md -mt-20 mx-auto p-10 gap-10">
+      <ContentDoc :value="data" class="prose prose-sm sm:prose-base dark:prose-invert max-w-none" />
     </div>
   </section>
 </template>
